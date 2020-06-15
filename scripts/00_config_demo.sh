5d07c16534b745b1301f23bdbe2adcf9ebe022d9
Manoj S <manoj.sundaram21@gmail.com>
2020-06-15 07:11:21 +0000

    updating jar file download locations

--- scripts/00_config_demo.sh
+++ scripts/00_config_demo.sh
@@ -6,6 +6,6 @@ sudo yum -y install unzip
 wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip
 unzip stanford-corenlp-full-2018-10-05.zip
 wget https://repository.cloudera.com/artifactory/cloudera-repos/org/apache/kudu/kudu-spark2_2.11/1.10.0-cdh6.3.3/kudu-spark2_2.11-1.10.0-cdh6.3.3.jar
-wget https://raw.githubusercontent.com/swordsmanliu/SparkStreamingHbase/master/lib/spark-core_2.11-1.5.2.logging.jar
-wget https://raw.githubusercontent.com/rajatrakesh/CDF-CDH-Workshop/master/scripts/start_nlp_engine.sh
+wget https://repository.cloudera.com/content/repositories/releases/org/apache/spark/spark-core_2.11/2.4.0-cdh6.3.2/spark-core_2.11-2.4.0-cdh6.3.2.jar
+wget https://raw.githubusercontent.com/manojsundaram/cdp-streaming-demo/master/scripts/start_nlp_engine.sh
 chmod +x *.sh
