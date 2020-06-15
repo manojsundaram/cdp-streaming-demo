d439ece25a5d7e9dd81e1edadd2a759fd76bb615
Manoj S <manoj.sundaram21@gmail.com>
2020-06-15 07:23:03 +0000

    updating scripts

--- scripts/00_config_demo.sh
+++ scripts/00_config_demo.sh
@@ -8,4 +8,4 @@ unzip stanford-corenlp-full-2018-10-05.zip
 wget https://repository.cloudera.com/artifactory/cloudera-repos/org/apache/kudu/kudu-spark2_2.11/1.10.0-cdh6.3.3/kudu-spark2_2.11-1.10.0-cdh6.3.3.jar
 wget https://repository.cloudera.com/content/repositories/releases/org/apache/spark/spark-core_2.11/2.4.0-cdh6.3.2/spark-core_2.11-2.4.0-cdh6.3.2.jar
 wget https://raw.githubusercontent.com/manojsundaram/cdp-streaming-demo/master/scripts/start_nlp_engine.sh
-chmod +x *.sh
+chmod +x /root/workflow-deployment/*.sh
